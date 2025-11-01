8f4eb30416eca053c76266cfb91da42bd463a62b
Usama Majid <usamamajid6@gmail.com>
2025-11-01 18:28:34 +0500

    ES Lint

---
+++ secondChance-backend/eslint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    semi: ['error', 'never'],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    eqeqeq: ['error', 'always'],
+  },
+}
