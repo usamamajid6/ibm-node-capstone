66c3d7fe57024ffe68f4a71cdc6b4a7c6a442760
Usama Majid <usamamajid6@gmail.com>
2025-11-01 12:23:27 +0500

    Auth routes

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -6,8 +6,7 @@ const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
-const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
-const searchRoutes = require('./routes/searchRoutes');
+
 
 
 
@@ -28,14 +27,15 @@ app.use(express.json());
 // Route files
 
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
-//{{insert code here}}
+const authRoutes = require('./routes/authRoutes');
+
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-app.use('/api/secondchance/items', secondChanceItemsRoutes);
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-app.use('/api/secondchance/search', searchRoutes);
+const searchRoutes = require('./routes/searchRoutes');
 
 
 
@@ -46,13 +46,13 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/auth', authRoutes);
 
 // Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/secondchance/search', searchRoutes);
 
 
 // Global Error Handler
